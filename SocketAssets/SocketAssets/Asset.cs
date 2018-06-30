e30debe3492ddecc6818b32f5336c6f763bd64ee
Beskorsova <tatyana.beskorsova@gmail.com>
2018-06-30 17:25:40 +0300

    parse exact types for each field

--- SocketAssets/SocketAssets/Asset.cs
+++ SocketAssets/SocketAssets/Asset.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,26 @@ namespace SocketAssets
 {
     public class Asset
     {
-       public string Name { get; set; }
-       public string Time { get; set; }
-       public string Bid { get; set; }
-       public string Ask { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public double Bid { get; set; }
+        public double Ask { get; set; }
+
+        private string dateFormat;
+
+        public Asset(string[] values, string dateFormat)
+        {
+            this.dateFormat = dateFormat;
+
+            Name = values[0];
+            Time = DateTime.ParseExact(values[1], dateFormat, CultureInfo.InvariantCulture);
+            Bid = double.Parse(values[2]);
+            Ask = double.Parse(values[3]);
+        }
 
         public override string ToString()
         {
-            return $"{Name} {Time} {Bid} {Ask}";
+            return $"{Name} {Time.ToString(dateFormat)} {Bid} {Ask}";
         }
     }
 }
--- SocketAssets/SocketAssets/Formatter/AssetFormatter.cs
+++ SocketAssets/SocketAssets/Formatter/AssetFormatter.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 
 namespace SocketAssets.Formatter
 {
-    class AssetFormatter: IFormatter<Asset>
+    class AssetFormatter : IFormatter<Asset>
     {
+        private readonly string dateFormat = "dd/MM/yyyy-HH:mm:ss.fff";
+
         public List<Asset> FromString(string result)
         {
             List<Asset> asserts = new List<Asset>();
@@ -12,10 +14,10 @@ namespace SocketAssets.Formatter
             string[] lineSeparators = new string[] { "\r\n" };
             char valueSplitter = ' ';
             string[] lines = result.Split(lineSeparators, StringSplitOptions.RemoveEmptyEntries);
-            foreach(var line in lines)
+            foreach (var line in lines)
             {
                 string[] values = line.Split(valueSplitter);
-                asserts.Add(new Asset() { Name = values[0], Time = values[1], Bid = values[2], Ask = values[3]  });
+                asserts.Add(new Asset(values, dateFormat));
             }
             return asserts;
         }
